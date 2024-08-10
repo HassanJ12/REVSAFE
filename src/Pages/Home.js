e279944e27e5dd950113e5769b6cc7c2bb4054c0
HassanJ12 <chishtihassan668@gmail.com>
2024-08-10 09:18:40 +0500

    Initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,83 @@
+import { Button, Typography } from "@material-tailwind/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleRight, faCheckDouble } from "@fortawesome/free-solid-svg-icons";
+import { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
+
+import Surveillance from "../Assets/1710586854274-removebg-preview.png";
+import AboutPage from "./About";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Change the animation duration as per your preference
+            once: true, // Set to true if you want the animation to occur only once
+        });
+    }, []);
+
+    return (
+        <div className="text-white">
+            <div className="min-h-screen flex flex-col">
+                <NavBar />
+                <div className="flex-grow flex flex-col  lg:flex-row justify-center">
+                    <div className="flex-1 flex flex-col gap-8 lg:gap-10 px-8 lg:px-12 py-16 lg:py-36 text-justify lg:text-left transition-all duration-400">
+                        <div className="flex flex-col gap-10 lg:gap-12">
+                            <Typography
+                                variant="h1"
+                                className="text-center lg:text-left text-3xl lg:text-4xl 2xl:text-6xl transition-all duration-400"
+                                style={{ textShadow: "0px 1px 2px rgba(255, 255, 255, 0.5)" }}
+                                data-aos="fade-right"
+                            >
+                                Track <span className="text-green-500">Positive</span>, <span className="text-red-700">Negative</span>{" "}
+                                <span className="text-[#3ae3a6]">Real & Fake</span>
+                            </Typography>
+                            <Typography data-aos="fade-left">
+                                Sentiment Analysis is a process of computationally identifying and categorizing opinions expressed in a piece of text, determining whether the sentiment expressed is positive, negative, or neutral. It's widely used in understanding customer feedback, social media monitoring, and market research. A Fake Review Model and System employs advanced algorithms to detect and filter out fraudulent reviews from genuine ones.
+                            </Typography>
+                        </div>
+                        <div className="flex items-center justify-center gap-12 lg:gap-20 lg:justify-start px-16 lg:px-0 transition-all duration-400">
+                            <div className="flex gap-2 items-center">
+                                <FontAwesomeIcon icon={faCheckDouble} color="green" />
+                                <Typography>Positive</Typography>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FontAwesomeIcon icon={faCheckDouble} color="green" />
+                                <Typography>Negative</Typography>
+                            </div>
+                            <div className="flex gap-2 items-center">
+                                <FontAwesomeIcon icon={faCheckDouble} color="green" />
+                                <Typography>Real</Typography>
+                            </div>
+                        </div>
+                        <div className="flex justify-center lg:justify-start">
+                            <Button onClick={()=>navigate("/price")} data-aos="zoom-in" className="text-gray-900 !bg-[linear-gradient(270deg,#3ae3a6,#2bb2ff)]">
+                                GET STARTED <FontAwesomeIcon icon={faAngleDoubleRight} className="pl-2" />
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="flex-1 flex items-center justify-center">
+                        <img
+                            src={Surveillance}
+                            alt=""
+                            className="w-[28rem] max-w-3xl p-8 md:p-4 lg:p-0 transition-all duration-400"
+                            // style={{ filter: "drop-shadow(0px 4px 4px rgba(255, 255, 255, 0.5))" }}
+                            data-aos="fade-up"
+                        />
+                    </div>
+                </div>
+                <AboutPage/>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/Pages/Price.js
@@ -0,0 +1,144 @@
+import React from 'react'
+import NavBar from '../Components/NavBar'
+import Footer from '../Components/Footer'
+import { SignUpModal } from '../Components/ModalSignUp'
+import { SignUpModalPrice } from '../Components/ModalSignUpPrice'
+
+export default function Price() {
+  return (
+    <>
+	 <NavBar />
+      <div className='min-h-screen flex justify-center items-center'>
+		
+    <div className='flex flex-wrap gap-4'>
+
+    <div class="card m-3">
+<div class="header">
+						<span class="title">Silver</span>
+						<span class="price">$29.99</span>
+					</div>
+					<p class="desc">Features:</p>
+					<ul class="lists">
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Detect fake reviews in up to 5 CSV files per month.Detect fake reviews in up to 5 CSV files per month.</span>
+						</li>
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Perform sentiment analysis on reviews in those CSV files.</span>
+						</li>
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Detailed report on review authenticity and sentiment scores.</span>
+						</li>
+					</ul>
+					<SignUpModalPrice/>
+				</div>
+                <div class="card m-3">
+<div class="header">
+						<span class="title">Gold</span>
+						<span class="price">$59.99 &#x24;</span>
+					</div>
+					<p class="desc">Features:</p>
+					<ul class="lists">
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Detect fake reviews in up to 15 CSV files per month.</span>
+						</li>
+
+                    	<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Perform sentiment analysis on reviews in those CSV files.</span>
+						</li>
+                        <li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Detailed report on review authenticity and sentiment scores.</span>
+						</li>
+
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Priority email support.</span>
+						</li>
+						{/* <li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Tristique enim nec</span>
+						</li> */}
+					</ul>
+					
+					<SignUpModalPrice/>
+				</div>
+			
+                <div class="card m-3">
+<div class="header">
+						<span class="title">Platinum</span>
+						<span class="price">$99.99 &#x24;</span>
+					</div>
+					<p class="desc">Features:</p>
+					<ul class="lists">
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Detect fake reviews in up to 30 CSV files per month.</span>
+						</li>
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Perform sentiment analysis on reviews in those CSV files.</span>
+						</li>
+						<li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Detailed report on review authenticity and sentiment scores.</span>
+						</li>
+                        <li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Priority email support.</span>
+						</li>
+                        <li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>New Feature: Check product reviews directly through a link without needing to upload a CSV file.</span>
+						</li>
+                        <li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Real-time review analysis and reporting.</span>
+						</li>
+                        {/* <li class="list">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+								<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+							</svg>
+							<span>Aenean quis</span>
+						</li> */}
+					</ul>
+					<SignUpModalPrice/>
+				</div>
+    </div>
+      </div>
+	  <Footer />
+    </>
+  )
+}
